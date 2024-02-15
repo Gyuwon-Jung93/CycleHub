33ca80e27adaf762b4c98eb673bfc4619241a0d5
gyuwon <kyn00018@gmail.com>
2024-02-15 23:59:36 +0000

    Committing Google maps markers+clustering

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener('DOMContentLoaded', function () {
     initMap();
 });
 
-
 async function initMap() {
+    let map;
+    let markers = [];
+    let locations = [
+        { lat: 53.349562, lng: -6.278198 },
+        { lat: 53.3537415547453, lng: -6.26530144781526 },
+        { lat: 53.336021, lng: -6.26298 },
+        { lat: 53.359405, lng: -6.276142 },
+        { lat: 53.336597, lng: -6.248109 },
+        { lat: 53.33796, lng: -6.24153 },
+        { lat: 53.343368, lng: -6.27012 },
+        { lat: 53.334123, lng: -6.265436 },
+        { lat: 53.344304, lng: -6.250427 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.338755, lng: -6.262003 },
+        { lat: 53.347777, lng: -6.244239 },
+        { lat: 53.336074, lng: -6.252825 },
+        { lat: 53.330091, lng: -6.268044 },
+        { lat: 53.350929, lng: -6.265125 },
+        { lat: 53.341515, lng: -6.256853 },
+        { lat: 53.344603, lng: -6.263371 },
+        { lat: 53.340927, lng: -6.262501 },
+        { lat: 53.348279, lng: -6.254662 },
+        { lat: 53.35893, lng: -6.280337 },
+        { lat: 53.357841, lng: -6.251557 },
+        { lat: 53.344115, lng: -6.237153 },
+        { lat: 53.343893, lng: -6.280531 },
+        { lat: 53.347477, lng: -6.28525 },
+    ];
 
-    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-        "marker",
-      );
-      const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    //Googlemaps loading
     const mapDiv = document.getElementById('map');
-    const mapCenter = {lat:53.3053398, lng:-6.2311915}; 
+    const mapCenter = { lat: 53.3053398, lng: -6.2311915 };
     if (mapDiv) {
-        var map = new google.maps.Map(mapDiv, {
+        map = new google.maps.Map(mapDiv, {
             center: mapCenter,
-            zoom: 13
+            zoom: 13,
         });
     }
-   
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-   
-  // Add some markers to the map.
-  const locations=[{ lat: 53.353227, lng: -6.2538856 },{ lat: 53.3419812, lng: -6.289286 },{ lat:53.3268839, lng: -6.2978491 }];
-  
-    const markers = locations.map((position, i) => {
-    const label = labels[i % labels.length];
-    const pinGlyph = new google.maps.marker.PinElement({
-      glyph: label,
-      glyphColor: "white",
+    // Add some markers to the map.
+    for (var i = 0; i < locations.length; i++) {
+        var marker = new google.maps.Marker({
+            map: map,
+            position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+        });
+        markers.push(marker);
+    }
+    //marker cluster
+    let markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+        gridSize: 70,
     });
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-      position,
-      content: pinGlyph.element,
-    });
-// markers can only be keyboard focusable when they have click listeners
-    // open info window when marker is clicked
-    marker.addListener("click", () => {
-        infoWindow.setContent(position.lat + ", " + position.lng);
-        infoWindow.open(map, marker);
-      });
-      return marker;
-
-      // Add a marker clusterer to manage the markers.
-
-    
-})
-new MarkerClusterer({ markers, map });
 }
