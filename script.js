54e0fbfda28e6d09881077f9557a1aea84a4fa42
gyuwon <kyn00018@gmail.com>
2024-02-15 09:30:18 +0000

    Committing Google maps change

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function initMap() {
-    var mapCenter = {lat:53.3085688, lng:-6.2910119}; // 서울의 위도와 경도
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: mapCenter
-    });
+    const mapDiv = document.getElementById('map');
+    const mapCenter = {lat:53.3053398, lng:-6.2311915}; 
+    if (mapDiv) {
+        var map = new google.maps.Map(mapDiv, {
+            center: mapCenter,
+            zoom: 13
+        });
+    }
 }
+
