70a60beb81778300edab71f8f1ffbc7389e76f66
Carlo <carlofinnegan2000@gmail.com>
2024-02-17 23:10:47 +0000

    creating sql tablke for stations

--- test.py
+++ test.py
@@ -23,5 +23,31 @@ engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/".format(USER, PASSWORD, URI
 # Create the database if it does not exist
 sql_create_db = text("CREATE DATABASE IF NOT EXISTS {};".format(DB))
 
+engine = create_engine(f"mysql+mysqldb://{USER}:{PASSWORD}@{URI}:{PORT}/{DB}", echo=True)
+
+
 engine.connect().execute(sql_create_db)
 
+for res in engine.connect().execute(text("SHOW VARIABLES;")):
+    print(res)
+
+sql = text("""
+CREATE TABLE IF NOT EXISTS station(
+    address VARCHAR(256),
+    banking INTEGER,
+    bike_stands INTEGER,
+    bonus INTEGER,
+    contract_name VARCHAR(256),
+    name VARCHAR(256),
+    number INTEGER,
+    position_lat REAL,
+    position_lng REAL,
+    status VARCHAR(256)
+);
+""")
+try:
+    res = engine.connect().execute(text("DROP TABLE IF EXISTS station"))
+    res = engine.connect().execute(sql)
+    print(res.fetachall())
+except Exception as e:
+    print(e)
