e0c72354ec3138b814f6dca4dcfd9181a24d2edd
Carlo <carlofinnegan2000@gmail.com>
2024-02-18 00:02:14 +0000

    basic table creation sql

--- test.py
+++ test.py
@@ -28,26 +28,54 @@ engine = create_engine(f"mysql+mysqldb://{USER}:{PASSWORD}@{URI}:{PORT}/{DB}", e
 
 engine.connect().execute(sql_create_db)
 
-for res in engine.connect().execute(text("SHOW VARIABLES;")):
-    print(res)
 
-sql = text("""
+station_table = text("""
 CREATE TABLE IF NOT EXISTS station(
+    station_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    station_number INTEGER,
+    name VARCHAR(256),
     address VARCHAR(256),
     banking INTEGER,
-    bike_stands INTEGER,
     bonus INTEGER,
+    overflow BOOLEAN,
     contract_name VARCHAR(256),
-    name VARCHAR(256),
-    number INTEGER,
     position_lat REAL,
-    position_lng REAL,
-    status VARCHAR(256)
+    position_lng REAL
 );
 """)
+
+
+
 try:
     res = engine.connect().execute(text("DROP TABLE IF EXISTS station"))
-    res = engine.connect().execute(sql)
+    res = engine.connect().execute(station_table)
     print(res.fetachall())
 except Exception as e:
     print(e)
+
+
+availability_table = text("""
+CREATE TABLE IF NOT EXISTS availability(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    station_id INT UNIQUE, 
+    status VARCHAR(256),
+    connected BOOLEAN,
+    last_update DATETIME,               
+    bikes INTEGER,
+    stands INTEGER,
+    mechanical_bikes INTEGER,
+    electrical_bikes INTEGER,
+    electrical_internal_battery_bikes INTEGER,
+    electrical_removable_battery_bikes INTEGER
+                        
+);
+""")
+
+
+try:
+    res = engine.connect().execute(availability_table)
+    print(res.fetachall())
+except Exception as e:
+    print(e)
+
+
