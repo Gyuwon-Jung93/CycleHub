ef74770d9899edba80580374ea61bda9de7bab1a
Carlo <carlofinnegan2000@gmail.com>
2024-02-25 15:40:05 +0000

    small cronscript fix

--- cron_script_decaux.sh
+++ cron_script_decaux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ubuntu/miniconda3/bin/activate comp30830
-python /home/ubuntu/CycleHub/decaux_scraper.py
+python /home/ubuntu/CycleHub/aws_rds/decaux_scraper.py
                                      
--- cron_script_weather.sh
+++ cron_script_weather.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ubuntu/miniconda3/bin/activate comp30830
-python /home/ubuntu/CycleHub/openweather_scraper.py
+python /home/ubuntu/CycleHub/aws_rds/openweather_scraper.py
                                      
