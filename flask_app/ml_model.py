4b510d4cb203d44de389f00fe22f54d8982b2a4d
Carlo <carlofinnegan2000@gmail.com>
2024-04-01 11:30:39 +0100

    tidying stuff

---
+++ flask_app/ml_model.py
@@ -0,0 +1,92 @@
+import requests
+import datetime
+import pickle
+
+def fetch_weather_data(lat, lng):
+    
+    API_KEY = 'e09fe30aecb65a55bb36442eda372b92'
+    BASE_URL = f" https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lng}&appid={API_KEY}"
+
+    response = requests.get(BASE_URL)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+    
+weather_data = fetch_weather_data(53.3498,-6.2603)
+
+def fetch_decaux_data():
+    CONTRACT_NAME = "dublin"
+    API_KEY = '99d3e65801ab0bdae585264b25d443c5545365b5'
+    BASE_URL = f"https://api.jcdecaux.com/vls/v1/stations?contract={CONTRACT_NAME}&apiKey={API_KEY}"
+
+    response = requests.get(BASE_URL)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+    
+station_data = fetch_decaux_data()
+
+
+
+
+from datetime import datetime
+import pandas as pd
+
+datetimes = []
+temperatures = []
+
+weather = []
+
+for item in weather_data['list']:
+    dt = datetime.utcfromtimestamp(item['dt'])
+    temp = item['main']['temp'] - 273.15 
+    wind_speed = item['wind']['speed']
+    description = item['weather'][0]['description']
+    weather.append({'time_of_day': dt, 'wind_speed': wind_speed, 'temperature': temp, 'description': description})
+
+df = pd.DataFrame(weather)
+df.set_index('time_of_day', inplace=True)
+
+# Generate hourly datetime range
+start_date = df.index.min()
+end_date = df.index.max()
+hourly_range = pd.date_range(start=start_date, end=end_date, freq='H')
+
+# Reindex the DataFrame with hourly datetime range
+df = df.reindex(hourly_range)
+
+# Interpolate missing values
+df_interpolated = df.interpolate(method='linear')
+         
+
+df_interpolated['description'] = df_interpolated['description'].fillna(method='ffill')
+# Print the interpolated DataFrame
+df_interpolated.reset_index(inplace=True)
+df_interpolated.rename(columns={'index': 'time_of_day'}, inplace=True)
+
+
+
+station_df = [{'station_id': d['number'], 'bike_stands': d['bike_stands']} for d in station_data]
+
+# Creating DataFrame
+df = pd.DataFrame(station_df)
+
+
+df3 = df_interpolated.merge(df, how ="cross")
+df3
+df3['time_of_day'] = pd.to_datetime(df3['time_of_day'])
+df3['month'] = pd.to_datetime(df3['time_of_day']).dt.month
+df3['day'] = pd.to_datetime(df3['time_of_day']).dt.day
+df3['hour'] = pd.to_datetime(df3['time_of_day']).dt.hour
+df3['weekday_num'] = df3['time_of_day'].dt.weekday + 1  # Adding 1 to match the range 1-7 (Sunday = 7)
+
+
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
